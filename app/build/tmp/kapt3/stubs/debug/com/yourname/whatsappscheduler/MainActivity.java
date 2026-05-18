6ce005f62e354d666b9c608a26d300881e4d2a4e
de-favre <collinsmosyemi@gmail.com>
2026-05-18 10:06:51 +0300

    added feaatures on import features

--- app/build/tmp/kapt3/stubs/debug/com/yourname/whatsappscheduler/MainActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/yourname/whatsappscheduler/MainActivity.java
@@ -1,10 +1,11 @@
 package com.yourname.whatsappscheduler;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000Z\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\t\u0018\u00002\u00020\u0001:\u0001)B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0016\u0010\u001c\u001a\b\u0012\u0004\u0012\u00020\u00180\u00172\u0006\u0010\u001d\u001a\u00020\nH\u0002J\u0012\u0010\u001e\u001a\u00020\u001f2\b\u0010 \u001a\u0004\u0018\u00010!H\u0014J\b\u0010\"\u001a\u00020\u001fH\u0002J\b\u0010#\u001a\u00020\u001fH\u0002J\b\u0010$\u001a\u00020\u001fH\u0002J\b\u0010%\u001a\u00020\u001fH\u0002J\b\u0010&\u001a\u00020\u001fH\u0002J\b\u0010\'\u001a\u00020\u001fH\u0002J\b\u0010(\u001a\u00020\u001fH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u001c\u0010\b\u001a\u0010\u0012\f\u0012\n \u000b*\u0004\u0018\u00010\n0\n0\tX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001c\u0010\f\u001a\u0010\u0012\f\u0012\n \u000b*\u0004\u0018\u00010\r0\r0\tX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000e\u001a\u00020\u000fX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0010\u001a\u00020\u0011X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0012\u001a\u00020\u0011X\u0082.\u00a2\u0006\u0002\n\u0000R\u001c\u0010\u0013\u001a\u0010\u0012\f\u0012\n \u000b*\u0004\u0018\u00010\n0\n0\tX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0014\u001a\u0004\u0018\u00010\u0015X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0016\u001a\b\u0012\u0004\u0012\u00020\u00180\u0017X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0019\u001a\u00020\u001aX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u001b\u001a\u00020\u001aX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006*"}, d2 = {"Lcom/yourname/whatsappscheduler/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "btnPickDateTime", "Landroid/widget/Button;", "btnSchedule", "btnSelectContact", "btnViewScheduled", "contactPermissionLauncher", "Landroidx/activity/result/ActivityResultLauncher;", "", "kotlin.jvm.PlatformType", "contactSelectionLauncher", "Landroid/content/Intent;", "database", "Lcom/yourname/whatsappscheduler/data/AppDatabase;", "etMessage", "Landroid/widget/EditText;", "etPhoneNumber", "notificationPermissionLauncher", "selectedCalendar", "Ljava/util/Calendar;", "selectedRecipients", "", "Lcom/yourname/whatsappscheduler/MainActivity$Recipient;", "tvSelectedContact", "Landroid/widget/TextView;", "tvSelectedDateTime", "getRecipientsForScheduling", "phoneNumberText", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "openContactSelector", "requestContactsPermissionThenOpen", "requestNotificationPermissionIfNeeded", "scheduleMessage", "showDateTimePicker", "showSchedulingFailure", "updateSelectedRecipientsUi", "Recipient", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000l\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0015\u0018\u00002\u00020\u0001:\u0001>B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0016\u0010\u001f\u001a\b\u0012\u0004\u0012\u00020\u001b0\u001a2\u0006\u0010 \u001a\u00020\fH\u0002J\u0010\u0010!\u001a\u00020\"2\u0006\u0010#\u001a\u00020$H\u0002J\u0010\u0010%\u001a\u00020&2\u0006\u0010\'\u001a\u00020\fH\u0002J\u0012\u0010(\u001a\u00020\"2\b\u0010)\u001a\u0004\u0018\u00010*H\u0014J\b\u0010+\u001a\u00020\"H\u0002J\b\u0010,\u001a\u00020\"H\u0002J\u0016\u0010-\u001a\b\u0012\u0004\u0012\u00020\u001b0\u001a2\u0006\u0010.\u001a\u00020\fH\u0002J\u0016\u0010/\u001a\b\u0012\u0004\u0012\u00020\f0\u001a2\u0006\u00100\u001a\u00020\fH\u0002J\u0016\u00101\u001a\b\u0012\u0004\u0012\u00020\u001b0\u001a2\u0006\u0010.\u001a\u00020\fH\u0002J\u0016\u00102\u001a\b\u0012\u0004\u0012\u00020\u001b0\u001a2\u0006\u0010.\u001a\u00020\fH\u0002J\b\u00103\u001a\u00020\"H\u0002J\b\u00104\u001a\u00020\"H\u0002J\b\u00105\u001a\u00020\"H\u0002J\b\u00106\u001a\u00020\"H\u0002J\b\u00107\u001a\u00020\"H\u0002J\u001c\u00108\u001a\b\u0012\u0004\u0012\u00020\f0\u001a2\f\u00109\u001a\b\u0012\u0004\u0012\u00020\f0\u001aH\u0002J\b\u0010:\u001a\u00020\"H\u0002J\u001c\u0010;\u001a\u0004\u0018\u00010\f*\b\u0012\u0004\u0012\u00020\f0\u001a2\u0006\u0010<\u001a\u00020\fH\u0002J\u0014\u0010=\u001a\u0004\u0018\u00010\u001b*\b\u0012\u0004\u0012\u00020\f0\u001aH\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0006\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u001a\u0010\t\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\f0\u000b0\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001c\u0010\r\u001a\u0010\u0012\f\u0012\n \u000e*\u0004\u0018\u00010\f0\f0\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001c\u0010\u000f\u001a\u0010\u0012\f\u0012\n \u000e*\u0004\u0018\u00010\u00100\u00100\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0011\u001a\u00020\u0012X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0013\u001a\u00020\u0014X\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0015\u001a\u00020\u0014X\u0082.\u00a2\u0006\u0002\n\u0000R\u001c\u0010\u0016\u001a\u0010\u0012\f\u0012\n \u000e*\u0004\u0018\u00010\f0\f0\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0017\u001a\u0004\u0018\u00010\u0018X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0019\u001a\b\u0012\u0004\u0012\u00020\u001b0\u001aX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u001c\u001a\u00020\u001dX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u001e\u001a\u00020\u001dX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006?"}, d2 = {"Lcom/yourname/whatsappscheduler/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "btnImportContacts", "Landroid/widget/Button;", "btnPickDateTime", "btnSchedule", "btnSelectContact", "btnViewScheduled", "contactImportLauncher", "Landroidx/activity/result/ActivityResultLauncher;", "", "", "contactPermissionLauncher", "kotlin.jvm.PlatformType", "contactSelectionLauncher", "Landroid/content/Intent;", "database", "Lcom/yourname/whatsappscheduler/data/AppDatabase;", "etMessage", "Landroid/widget/EditText;", "etPhoneNumber", "notificationPermissionLauncher", "selectedCalendar", "Ljava/util/Calendar;", "selectedRecipients", "", "Lcom/yourname/whatsappscheduler/MainActivity$Recipient;", "tvSelectedContact", "Landroid/widget/TextView;", "tvSelectedDateTime", "getRecipientsForScheduling", "phoneNumberText", "importContacts", "", "uri", "Landroid/net/Uri;", "looksLikePhoneNumber", "", "value", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "openContactSelector", "openContactsFilePicker", "parseDelimitedContacts", "content", "parseDelimitedLine", "line", "parseImportedContacts", "parseVCardContacts", "requestContactsPermissionThenOpen", "requestNotificationPermissionIfNeeded", "scheduleMessage", "showDateTimePicker", "showSchedulingFailure", "unfoldVCardLines", "lines", "updateSelectedRecipientsUi", "firstValueFor", "propertyName", "toRecipientFromUnknownColumns", "Recipient", "app_debug"})
 public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
     private android.widget.EditText etPhoneNumber;
     private android.widget.EditText etMessage;
     private android.widget.Button btnSelectContact;
+    private android.widget.Button btnImportContacts;
     private android.widget.TextView tvSelectedContact;
     private android.widget.Button btnPickDateTime;
     private android.widget.TextView tvSelectedDateTime;
@@ -21,6 +22,8 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
     private final androidx.activity.result.ActivityResultLauncher<java.lang.String> contactPermissionLauncher = null;
     @org.jetbrains.annotations.NotNull
     private final androidx.activity.result.ActivityResultLauncher<android.content.Intent> contactSelectionLauncher = null;
+    @org.jetbrains.annotations.NotNull
+    private final androidx.activity.result.ActivityResultLauncher<java.lang.String[]> contactImportLauncher = null;
     
     public MainActivity() {
         super();
@@ -43,11 +46,49 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
     private final void openContactSelector() {
     }
     
+    private final void openContactsFilePicker() {
+    }
+    
+    private final void importContacts(android.net.Uri uri) {
+    }
+    
     private final void updateSelectedRecipientsUi() {
     }
     
     private final java.util.List<com.yourname.whatsappscheduler.MainActivity.Recipient> getRecipientsForScheduling(java.lang.String phoneNumberText) {
         return null;
+    }
+    
+    private final java.util.List<com.yourname.whatsappscheduler.MainActivity.Recipient> parseImportedContacts(java.lang.String content) {
+        return null;
+    }
+    
+    private final java.util.List<com.yourname.whatsappscheduler.MainActivity.Recipient> parseVCardContacts(java.lang.String content) {
+        return null;
+    }
+    
+    private final java.util.List<java.lang.String> unfoldVCardLines(java.util.List<java.lang.String> lines) {
+        return null;
+    }
+    
+    private final java.lang.String firstValueFor(java.util.List<java.lang.String> $this$firstValueFor, java.lang.String propertyName) {
+        return null;
+    }
+    
+    private final java.util.List<com.yourname.whatsappscheduler.MainActivity.Recipient> parseDelimitedContacts(java.lang.String content) {
+        return null;
+    }
+    
+    private final java.util.List<java.lang.String> parseDelimitedLine(java.lang.String line) {
+        return null;
+    }
+    
+    private final com.yourname.whatsappscheduler.MainActivity.Recipient toRecipientFromUnknownColumns(java.util.List<java.lang.String> $this$toRecipientFromUnknownColumns) {
+        return null;
+    }
+    
+    private final boolean looksLikePhoneNumber(java.lang.String value) {
+        return false;
     }
     
     private final void requestNotificationPermissionIfNeeded() {
--- app/src/main/java/com/yourname/whatsappscheduler/MainActivity.kt
+++ app/src/main/java/com/yourname/whatsappscheduler/MainActivity.kt
@@ -6,6 +6,7 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etPhoneNumber: EditText
     private lateinit var etMessage: EditText
     private lateinit var btnSelectContact: Button
+    private lateinit var btnImportContacts: Button
     private lateinit var tvSelectedContact: TextView
     private lateinit var btnPickDateTime: Button
     private lateinit var tvSelectedDateTime: TextView
@@ -69,6 +71,13 @@ class MainActivity : AppCompatActivity() {
             updateSelectedRecipientsUi()
         }
     }
+    private val contactImportLauncher = registerForActivityResult(
+        ActivityResultContracts.OpenDocument()
+    ) { uri ->
+        if (uri != null) {
+            importContacts(uri)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,6 +88,7 @@ class MainActivity : AppCompatActivity() {
         etPhoneNumber = findViewById(R.id.etPhoneNumber)
         etMessage = findViewById(R.id.etMessage)
         btnSelectContact = findViewById(R.id.btnSelectContact)
+        btnImportContacts = findViewById(R.id.btnImportContacts)
         tvSelectedContact = findViewById(R.id.tvSelectedContact)
         btnPickDateTime = findViewById(R.id.btnPickDateTime)
         tvSelectedDateTime = findViewById(R.id.tvSelectedDateTime)
@@ -87,6 +97,10 @@ class MainActivity : AppCompatActivity() {
 
         btnSelectContact.setOnClickListener {
             requestContactsPermissionThenOpen()
+        }
+
+        btnImportContacts.setOnClickListener {
+            openContactsFilePicker()
         }
 
         btnPickDateTime.setOnClickListener {
@@ -229,6 +243,45 @@ class MainActivity : AppCompatActivity() {
         contactSelectionLauncher.launch(Intent(this, ContactSelectionActivity::class.java))
     }
 
+    private fun openContactsFilePicker() {
+        contactImportLauncher.launch(
+            arrayOf(
+                "text/*",
+                "text/vcard",
+                "text/x-vcard",
+                "text/comma-separated-values",
+                "application/csv",
+                "application/vnd.ms-excel"
+            )
+        )
+    }
+
+    private fun importContacts(uri: Uri) {
+        val importedRecipients = runCatching {
+            contentResolver.openInputStream(uri)?.bufferedReader()?.use { reader ->
+                parseImportedContacts(reader.readText())
+            }.orEmpty()
+        }.getOrElse {
+            Toast.makeText(this, "Could not read contacts file.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if (importedRecipients.isEmpty()) {
+            Toast.makeText(this, "No valid phone numbers found in that file.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        selectedRecipients = (selectedRecipients + importedRecipients)
+            .distinctBy { PhoneNumberFormatter.toWhatsAppNumber(it.phoneNumber) }
+        updateSelectedRecipientsUi()
+
+        Toast.makeText(
+            this,
+            "Imported ${importedRecipients.size} contact(s).",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
     private fun updateSelectedRecipientsUi() {
         etPhoneNumber.setText(selectedRecipients.joinToString(", ") { it.phoneNumber })
         tvSelectedContact.text = when (selectedRecipients.size) {
@@ -247,6 +300,131 @@ class MainActivity : AppCompatActivity() {
             .filter { PhoneNumberFormatter.toWhatsAppNumber(it).isNotEmpty() }
             .distinct()
             .map { Recipient(it, it) }
+    }
+
+    private fun parseImportedContacts(content: String): List<Recipient> {
+        val trimmedContent = content.trim()
+        if (trimmedContent.isBlank()) return emptyList()
+
+        return if (trimmedContent.contains("BEGIN:VCARD", ignoreCase = true)) {
+            parseVCardContacts(trimmedContent)
+        } else {
+            parseDelimitedContacts(trimmedContent)
+        }.filter { PhoneNumberFormatter.toWhatsAppNumber(it.phoneNumber).isNotEmpty() }
+            .distinctBy { PhoneNumberFormatter.toWhatsAppNumber(it.phoneNumber) }
+    }
+
+    private fun parseVCardContacts(content: String): List<Recipient> {
+        return content.splitToSequence(Regex("(?i)BEGIN:VCARD"))
+            .mapNotNull { card ->
+                val lines = unfoldVCardLines(card.lines())
+                val name = lines.firstValueFor("FN")
+                    ?: lines.firstValueFor("N")?.replace(';', ' ')?.trim()
+                val phoneNumber = lines.firstValueFor("TEL")
+
+                phoneNumber?.takeIf { it.isNotBlank() }?.let {
+                    Recipient(name.orEmpty().ifBlank { it }, it)
+                }
+            }
+            .toList()
+    }
+
+    private fun unfoldVCardLines(lines: List<String>): List<String> {
+        val unfoldedLines = mutableListOf<String>()
+        lines.forEach { line ->
+            if ((line.startsWith(" ") || line.startsWith("\t")) && unfoldedLines.isNotEmpty()) {
+                unfoldedLines[unfoldedLines.lastIndex] += line.trimStart()
+            } else {
+                unfoldedLines.add(line)
+            }
+        }
+        return unfoldedLines
+    }
+
+    private fun List<String>.firstValueFor(propertyName: String): String? {
+        return firstNotNullOfOrNull { line ->
+            val key = line.substringBefore(":", missingDelimiterValue = "")
+                .substringBefore(";")
+                .uppercase()
+            if (key == propertyName) line.substringAfter(":", "").trim().ifBlank { null } else null
+        }
+    }
+
+    private fun parseDelimitedContacts(content: String): List<Recipient> {
+        val rows = content.lines()
+            .map { parseDelimitedLine(it) }
+            .filter { fields -> fields.any { it.isNotBlank() } }
+
+        if (rows.isEmpty()) return emptyList()
+
+        val header = rows.first()
+        val nameIndex = header.indexOfFirst {
+            it.equals("name", ignoreCase = true) ||
+                it.equals("contact", ignoreCase = true) ||
+                it.equals("full name", ignoreCase = true)
+        }
+        val phoneIndex = header.indexOfFirst {
+            it.equals("phone", ignoreCase = true) ||
+                it.equals("phone number", ignoreCase = true) ||
+                it.equals("mobile", ignoreCase = true) ||
+                it.equals("number", ignoreCase = true) ||
+                it.equals("tel", ignoreCase = true)
+        }
+        val hasHeader = phoneIndex >= 0
+        val dataRows = if (hasHeader) rows.drop(1) else rows
+
+        return dataRows.mapNotNull { fields ->
+            if (hasHeader) {
+                val phoneNumber = fields.getOrNull(phoneIndex).orEmpty().trim()
+                val name = fields.getOrNull(nameIndex).orEmpty().trim().ifBlank { phoneNumber }
+                phoneNumber.takeIf { it.isNotBlank() }?.let { Recipient(name, it) }
+            } else {
+                fields.toRecipientFromUnknownColumns()
+            }
+        }
+    }
+
+    private fun parseDelimitedLine(line: String): List<String> {
+        val separator = when {
+            line.contains(",") -> ','
+            line.contains(";") -> ';'
+            line.contains("\t") -> '\t'
+            else -> null
+        }
+        if (separator == null) return listOf(line.trim())
+
+        val fields = mutableListOf<String>()
+        val currentField = StringBuilder()
+        var insideQuotes = false
+        line.forEach { char ->
+            when {
+                char == '"' -> insideQuotes = !insideQuotes
+                char == separator && !insideQuotes -> {
+                    fields.add(currentField.toString().trim())
+                    currentField.clear()
+                }
+                else -> currentField.append(char)
+            }
+        }
+        fields.add(currentField.toString().trim())
+        return fields
+    }
+
+    private fun List<String>.toRecipientFromUnknownColumns(): Recipient? {
+        val phoneIndex = indexOfFirst { looksLikePhoneNumber(it) }
+        if (phoneIndex < 0) return null
+
+        val phoneNumber = get(phoneIndex).trim()
+        val name = filterIndexed { index, value ->
+            index != phoneIndex && value.isNotBlank()
+        }.joinToString(" ").ifBlank { phoneNumber }
+
+        return Recipient(name, phoneNumber)
+    }
+
+    private fun looksLikePhoneNumber(value: String): Boolean {
+        val digitCount = value.count { it.isDigit() }
+        return digitCount >= 7 && PhoneNumberFormatter.toWhatsAppNumber(value).isNotEmpty()
     }
 
     private fun requestNotificationPermissionIfNeeded() {
